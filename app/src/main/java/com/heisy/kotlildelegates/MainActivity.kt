2471508f1b34ed1d89f25f3f02cac990c6dc9ec5
Akjoi <tihonof77777@yandex.ru>
2022-05-25 19:01:05 +0300

    delegates for Fragment

--- app/src/main/java/com/heisy/kotlildelegates/MainActivity.kt
+++ app/src/main/java/com/heisy/kotlildelegates/MainActivity.kt
@@ -2,10 +2,31 @@ package com.heisy.kotlildelegates
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        class TrimDelegate : ReadWriteProperty<Any?, String> {
+
+            private var trimmedValue: String = ""
+
+            override fun getValue(
+                thisRef: Any?,
+                property: KProperty<*>
+            ): String {
+                return trimmedValue
+            }
+
+            override fun setValue(
+                thisRef: Any?,
+                property: KProperty<*>, value: String
+            ) {
+                trimmedValue = value.trim()
+            }
+        }
     }
 }
